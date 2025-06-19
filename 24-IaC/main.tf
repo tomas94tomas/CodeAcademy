4674bf87bf933ba9021385b51524e64306c3badd
Tomas T. <tomas011@gmail.com>
2025-06-19 21:44:03 +0300

    24 terraform tasks added

---
+++ 24-IaC/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "TomasRG2" {
+  name     = var.resource_group_name
+  location = var.location
+}
---
+++ 24-IaC/outputs.tf
@@ -0,0 +1,3 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.TomasRG2.name
+}
